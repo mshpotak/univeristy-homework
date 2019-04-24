9ccf29adb10a662a491edf453a771b1eaf3024b6
mshpotak <mshpotak@ukr.net>
2019-04-24 18:31:30 +0200

    Revised lab4part1, lab4part2

--- vountesmery-digitech-unix/lab4part1.c
+++ vountesmery-digitech-unix/lab4part1.c
@@ -18,10 +18,19 @@
 
 // 2. Описує глобальний дескриптор файла логу.
 
+char path[] = "/home/mykhailo/github/university-homework/vountesmery-digitech-unix/";
 int fd_log_prog;
 int fd_log_serv;
 
-// 1. Кожна дія сервера супроводжується відміткою у логу.
+char* set_path( char* path, char* file_name){
+    int p_len = strlen(path);
+    int fn_len = strlen(file_name);
+    char* file_path = (char*)calloc(p_len+fn_len, sizeof(char));
+    strcpy(file_path, path);
+    strcat(file_path, file_name);
+    return file_path;
+}
+
 void log_entry( int fd_log, const char* note ){
     int result;
     time_t log_timestamp;
@@ -41,219 +50,197 @@ void log_entry( int fd_log, const char* note ){
         }
         goto retry;
     }
+    printf("%.8s %s\n", ctime( &log_timestamp ) + 11, note);
     return;
 }
 
-int daemonize(){
-    int result;
-    pid_t pid_main = getpid();
-    pid_t pid_current;
-    pid_t pid_daemon;
-
-    result = fork();
-    if( result == -1 ){
-        perror( "fork error:" );
-        if( close(fd_log_prog) == -1 ){
-            perror( "close error:" );
-        };
-        return -1;
-    } else if ( result == 0){
-        pid_daemon = getpid();
-    } else {
-        pid_daemon = result;
-    }
-    pid_current = getpid();
-
-
-    if(pid_current == pid_main){
-        log_entry( fd_log_prog, "fork process started" );
-        log_entry( fd_log_prog, "main process closed" );
-        return 0;
-    }
-
-    pid_t sid_daemon = setsid();
-    if( sid_daemon == -1 ){
-        perror("setsid error:");
-    }
-
-    pid_main = pid_daemon;
-    result = fork();
-    if( result == -1 ){
-        perror("fork error:");
-        if( close( fd_log_prog ) == -1 ){
-            perror("close error:");
-        };
-        return -1;
-    } else if ( result == 0 ){
-        pid_daemon = getpid();
-    } else {
-        pid_daemon = result;
-    }
-    pid_current = getpid();
-
-    if( pid_current == pid_main ){
-        log_entry( fd_log_prog, "daemon process started" );
-        log_entry( fd_log_prog, "fork process closed" );
-        return 0;
-    }
-
-    if( chdir("/") == -1 ){
-        perror("chdir error:");
-        if( close(fd_log_prog) == -1 ){
-            perror("close error:");
-        };
-        return -1;
-    }
-
-    if( close( fd_log_prog ) == -1) {
-        perror("close fd_log_prog error:");
-        return -1;
-    };
-    if( close(0) == -1 ){
-        perror("close stdin error:");
-        return -1;
-    };
-    if( close(1) == -1 ){
-        perror("close stdout error:");
-        return -1;
-    };
-    if( close(2) == -1 ){
-        perror("close stderr error:");
-        return -1;
-    };
-
-    freopen( "/dev/null", "w+", stdin );
-    freopen( "/dev/null", "w+", stdout );
-    freopen( "/dev/null", "w+", stderr );
-
-    fd_log_prog = open( "/home/mykhailo/github/university-homework/vountesmery-digitech-unix/log_program.txt", O_WRONLY|O_APPEND);
-    if( fd_log_prog == -1 ){
-        perror("open log as daemon error:");
-        return -1;
-    }
-
-    log_entry(fd_log_prog, "daemonize() succesfull");
-    return pid_daemon;
-}
-
-void wait_for_connection(int fd_serv){
+int wait_for_connection(int fd_serv){
     int result;
     struct pollfd sfd;
     sfd.fd = fd_serv;
     sfd.events = POLLIN;
-
     //wait until data is available
     while(1){
-        result = poll(&sfd, 1, 1000);
+        result = poll(&sfd, 1, 5000);
+        if(result == 0){
+            //printf("%.8s No connections found...\n", ctime( &time_current ) + 11 );
+            continue;
+        }
         if(result == -1){
             perror("poll error:");
-            continue;
+            return -1;
         };
         if(result > 0){
             //check for error
             if(sfd.revents & POLLERR){
-                perror("poll revents error:");
+                //perror("poll revents error:");
                 continue;
             }
             //recv() if data is available
             if(sfd.revents & POLLIN){
                 result = fork();
-                if( result == 1 ) {
-                    continue;
+                if( result == 0 ) {
+                    return 0;
                 } else {
-                    close(fd_serv);
-                    return;
+                    //continue;
+                    exit(0);
                 }
             }
         }
     }
+    return 1;
+}
+
+int wait_for_msg(int fd_serv, double timeout){
+    int result;
+    struct pollfd sfd;
+    sfd.fd = fd_serv;
+    sfd.events = POLLIN;
+    time_t time_start, time_current;
+    time( &time_start );
+    //wait until data is available
+    time( &time_current );
+    while( (timeout == 0) || ( difftime( time_current, time_start ) < timeout ) ){
+        result = poll(&sfd, 1, 5000);
+        time( &time_current );
+        if(result == 0){
+            printf("%.8s No connections found...\n", ctime( &time_current ) + 11 );
+            continue;
+        }
+        if(result == -1){
+            perror("poll error:");
+            return -1;
+        };
+        if(result > 0){
+            //check for error
+            if(sfd.revents & POLLERR){
+                //perror("poll revents error:");
+                continue;
+            }
+            if(sfd.revents & POLLIN){
+                return 0;
+
+            }
+        }
+    }
+    return 1;
+}
+
+
+void add_prefix( char* msg ){
+    char temp[256];
+    pid_t pid = getpid();
+    time_t timestamp = time(&timestamp);
+    memset( temp, '\0', 256);
+    sprintf( temp, "PID#%d; %.24s; %s", pid, ctime(&timestamp), msg);
+    memcpy( msg, temp, 256);
 }
 
 int main( int argc, char *argv[] ){
     int result;
-    pid_t pid_prog = 0;
+    char* file_path = set_path( path,  "log_program.txt" );
 
-    fd_log_prog = open( "log_program.txt", O_CREAT|O_TRUNC|O_RDWR, 0644 );
-    // 1. Виконує демонізацію, подальший функціонал стосується демона.
-    // pid_prog = daemonize();
-    // if( (pid_prog == 0) || (pid_prog < 0) ){
-    //     return 0;
-    // }
-    // // 3. Описує структуру struct sockaddr_in з параметрами: формат сокетів - PF_INET,
-    // // адреса - будь яка, порт - 3200 + номер варіанта.
+    fd_log_prog = open( file_path, O_CREAT|O_TRUNC|O_RDWR, 0644 );
+    log_entry( fd_log_prog, "program log created" );
 
     struct sockaddr_in address;
     socklen_t addrlen = sizeof( struct sockaddr_in );
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = htonl( INADDR_ANY );
     address.sin_port = htons( PORT );
     memset( &( address.sin_zero ), '\0', 8 );
 
-    // // 4. Формує сокет типу SOCK_STREAM формату PF_INET.
     int fd_serv;
     fd_serv = socket( PF_INET, SOCK_STREAM, 0);
+    if( fd_serv == -1 ){
+        perror( "socket() error:" );
+        log_entry( fd_log_prog, strerror( errno ) );
+        return -1;
+    }
     log_entry( fd_log_prog, "socket created" );
 
-    // // 5. Налаштовує сокет на очікування запитів за допомогою bind.
-    result = bind( fd_serv, (struct sockaddr *)&address, addrlen);
-    log_entry( fd_log_prog, "bind created" );
-    // // 6. Запускає нескінченний цикл обробки запитів від клієнтів.
-    int fd_client;
-    pid_t pid_client;
-    time_t curtime;
-    int hname_size = 64;
-    char hname [ hname_size ];
-    int buffer_size = 256;
-    char buffer [ buffer_size ];
-    int bytes_read;
-    int bytes_sent;
-    // char send_buffer[256];
-    // char recv_buffer[256];
-    result = listen( fd_serv, BACKLOG );
-    printf("Hey0");
-    log_entry( fd_log_prog, "started listening" );
-    printf("Hey1");
-    fd_log_serv = open( "/home/mykhailo/github/university-homework/vountesmery-digitech-unix/log_server.txt", O_CREAT|O_TRUNC|O_RDWR, 0644);
-    printf("Hey2");
-    while(1){
-        // 7. На кожний запит виконує fork для породження процесу обробки.
-        wait_for_connection(fd_serv);
-        log_entry( fd_log_serv, "connection found" );
-
-        fd_client = accept( fd_serv, (struct sockaddr *)&address, &addrlen );
-        log_entry( fd_log_serv, "opened connection" );
-
-        pid_client = getpid();
-        //sends welcome message to the new client
-        time( &curtime );
-        gethostname( hname, hname_size );
-        sprintf( buffer, "\t\t-- Welcome to the server, %s! --\nPID#%dSERV_PID#%d; %.24s; Connection opened.\n", hname, pid_prog, pid_client, ctime( &curtime ) );
-        send( fd_client, buffer, strlen( buffer ), 0 );
-        log_entry( fd_log_serv, "welcome message sent" );
-        // 9. Процес обробки в нескінченному циклі отримує від клієнта строки за
-        // допомогою recv, додає до них свій префікс у вигляді поточного часу та
-        // власного pid, та повертає клієнту за допомогою send.
-        while(1){
-            bytes_read = recv( fd_client, buffer, buffer_size, 0 );
-            log_entry( fd_log_serv, "message recieved" );
-            // 10. Процес обробки завершує обробку даних від клієнта отримавши строку
-            // "close".
-            if( strcmp( buffer, "close" ) == 0 ){
-                time( &curtime );
-                sprintf( buffer, "PID#%dSERV_PID#%d; %.24s; Connection closed.\n\t\t-- Goodbye, %s! --\n", pid_prog, pid_client, ctime( &curtime ), hname );
-                bytes_sent = send( fd_client, buffer, strlen( buffer ), 0);
-                close( fd_client);
-                log_entry( fd_log_serv, "connection closed" );
-                log_entry( fd_log_prog, "connection closed" );
-                break;
-            }
-
-            time( &curtime);
-            sprintf( buffer, "PID#%dSERV_PID#%d; %.24s; %s!\n", pid_prog, pid_client, ctime( &curtime ), buffer );
-            bytes_sent = send( fd_client, buffer, strlen( buffer ), 0);
-        }
+    int option = 1;
+    result = setsockopt( fd_serv, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int) );
+    if( result == -1 ){
+        perror( "setsockopt() error:" );
+        log_entry( fd_log_prog, strerror( errno ) );
+        return -1;
     }
-    // 8. Батьківський процес закриває сокет.
+    log_entry( fd_log_prog, "address reuse enabled" );
 
+    result = bind( fd_serv, (struct sockaddr *)&address, addrlen );
+    if( result == -1 ){
+        perror( "bind() error:" );
+        log_entry( fd_log_prog, strerror(errno) );
+        return -1;
+    }
+    log_entry( fd_log_prog, "bind created" );
+
+    result = listen( fd_serv, BACKLOG );
+    if( result == -1 ){
+        perror( "listen() error:" );
+        log_entry( fd_log_prog, strerror(errno) );
+        return -1;
+    }
+    log_entry( fd_log_prog, "started listening" );
+
+    file_path = set_path( path, "log_server.txt" );
+    fd_log_serv = open( file_path, O_CREAT|O_TRUNC|O_RDWR, 0644);
+    log_entry( fd_log_prog, "server log created" );
+    log_entry( fd_log_serv, "server log created" );
+
+    log_entry( fd_log_serv, "connection polling started" );
+    result = wait_for_connection( fd_serv );
+    if( result == -1 ){
+        perror("polling error:");
+        return 0;
+    }
+    log_entry( fd_log_serv, "connection found" );
+
+    int fd_client;
+    fd_client = accept( fd_serv, (struct sockaddr *)&address, &addrlen );
+    close(fd_serv);
+
+    if( fd_client == -1 ){
+        perror( "accept() error:" );
+        log_entry( fd_log_prog, strerror( errno ) );
+        return -1;
+    }
+    log_entry( fd_log_serv, "connection opened" );
+
+    char buffer[256] = {"\n\t\t----------------------------\n\t\t-- Welcome to the server! --\n\t\t----------------------------\n"};
+
+    result = send( fd_client, buffer, strlen( buffer ), 0 );
+    log_entry( fd_log_serv, "welcome msg sent" );
+
+    while( 1 ){
+        result = wait_for_msg( fd_client, 0 );
+        if( result == 1 ){
+            log_entry( fd_log_serv, "timeout: no client actvity detected" );
+            close( fd_client );
+            log_entry( fd_log_serv, "connection closed" );
+            break;
+        }
+
+        memset( buffer, '\0', 256 );
+        result = recv( fd_client, buffer, 256, 0 );
+        log_entry( fd_log_serv, "msg recieved" );
+        if( strcmp( buffer, "close" ) == 0 ){
+            add_prefix( buffer );
+            log_entry( fd_log_serv, buffer);
+            strcat( buffer, "\n\t\t----------------------------\n\t\t--------- Goodbye! ---------\n\t\t----------------------------\n");
+            result = send( fd_client, buffer, strlen(buffer), 0);
+            break;
+        }
+
+        add_prefix( buffer );
+        result = send( fd_client, buffer, strlen(buffer), 0);
+        log_entry( fd_log_serv, buffer);
+    }
+
+    close(fd_client);
+    close(fd_serv);
+    close(fd_log_serv);
+    close(fd_log_prog);
     return 0;
 }
--- vountesmery-digitech-unix/lab4part2.c
+++ vountesmery-digitech-unix/lab4part2.c
@@ -15,84 +15,122 @@
 
 #define PORT 3210
 
+int wait_for_msg(int fd_serv, double timeout){
+    int result;
+    struct pollfd sfd;
+    sfd.fd = fd_serv;
+    sfd.events = POLLIN;
+    time_t time_start, time_current;
+    time( &time_start );
+    //wait until data is available
+    time( &time_current );
+    while( (timeout == 0) || ( difftime( time_current, time_start ) < timeout ) ){
+        result = poll(&sfd, 1, 5000);
+        time( &time_current );
+        if(result == 0){
+            printf("%.8s No connections found...\n", ctime( &time_current ) + 11 );
+            continue;
+        }
+        if(result == -1){
+            perror("poll error:");
+            return -1;
+        };
+        if(result > 0){
+            //check for error
+            if(sfd.revents & POLLERR){
+                //perror("poll revents error:");
+                continue;
+            }
+            //recv() if data is available
+            if(sfd.revents & POLLIN){
+                return 0;
+                // result = fork();
+                // if( result == 0 ) {
+                //     close(fd_serv);
+                //     return;
+                // } else {
+                //     continue;
+                // }
+            }
+        }
+    }
+    return 1;
+}
+
 int main(){
     int result;
-    int buffer_size = 256;
-    char buffer [ buffer_size ];
+    time_t time_current;
 
-    // 1.Описує структуру struct sockaddr_in з параметрами: формат сокетів - PF_INET,
-    // адреса - обчислена за викликом htonl(INADDR_LOOPBACK), порт - 3200 +
-    // номер варіанта.
     struct sockaddr_in address;
     socklen_t addrlen = sizeof( struct sockaddr_in );
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    address.sin_addr.s_addr = htonl( INADDR_LOOPBACK );
     address.sin_port = htons( PORT );
     memset( &( address.sin_zero ), '\0', 8 );
 
-    // 2. Формує сокет типу SOCK_STREAM формату PF_INET.
     int fd_serv;
     fd_serv = socket( PF_INET, SOCK_STREAM, 0);
-
-    // 3. Налаштовує сокет на підключення до сервера за допомогою connect.
-    result = connect( fd_serv, (struct sockaddr *)&address, addrlen);
-
-    // 4. В нескінченному циклі запитує рядки від оператора, передає їх на сервер та
-    // друкує отримані відповіді.
-    int death = 0;
-    result = fork();
-    if(result == 0){
-        //pid_recv = getpid();
-        struct pollfd sfd;
-        sfd.fd = fd_serv;
-        sfd.events = POLLIN;
-        while(1){
-
-            if((result = poll(&sfd, 1, 1000)) == -1){
-                perror("poll error:");
-                continue;
-            };
-            if(result > 0){
-                //check for error
-                if(sfd.revents & POLLERR){
-                    perror("poll revents error:");
-                    continue;
-                }
-                //recv() if data is available
-                if(sfd.revents & POLLIN){
-                    if(recv(fd_serv, buffer, buffer_size, 0) == -1){
-                        perror("receive error:");
-                        continue;
-                    }
-                    printf("Server:\t%s\n", buffer);
-                    if(death == 1){
-                        return 0;
-                    };
-                }
-            }
-        }
-    } else {
-        //pid_send = getpid();
-        while(1){
-            printf("Input a string:\t");
-            result = scanf("%[^\n]s", buffer);
-            if(result == -1){
-                perror("scanf() error:");
-                continue;
-            }
-            if(result > buffer_size){
-                printf("The string is too big. The number of symbols must be less then %d.\n", buffer_size);
-                continue;
-            }
-
-            send(fd_serv, buffer, strlen(buffer), 0);
-            if( strcmp( buffer, "close" ) == 0){
-                death = 1;
-                return 0;
-            }
-            // 5. Робота закінчується після вводу оператором рядка "close" та отримання на
-            // нього відповіді від сервера.
-        }
+    if( fd_serv == -1 ){
+        perror( "socket() error:" );
+        return -1;
     }
+    time(&time_current);
+    printf("%.8s Socket created\n", ctime( &time_current ) + 11 );
+
+    int option = 1;
+    result = setsockopt( fd_serv, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int) );
+    if( result == -1 ){
+        perror( "setsockopt() error:" );
+        return -1;
+    }
+    time(&time_current);
+    printf("%.8s Address reuse enabled\n", ctime( &time_current ) + 11 );
+
+    result = connect( fd_serv, (struct sockaddr *)&address, addrlen );
+    if( result == -1 ){
+        perror( "connect() error:" );
+        return -1;
+    }
+    time(&time_current);
+    printf("%.8s Connection requested\n", ctime( &time_current ) + 11 );
+
+    char buffer [256];
+    while(1){
+        result = wait_for_msg( fd_serv, 5 );
+        if( result == 0 ){
+            if( strcmp( buffer, "close" ) == 0 ){
+                memset( buffer, '\0', 256 );
+                result = recv( fd_serv, buffer, 256, 0 );
+                time( &time_current );
+                printf( "%.8s Server: %s\n", ctime( &time_current ) + 11, buffer );
+                break;
+            }
+            memset( buffer, '\0', 256 );
+            result = recv( fd_serv, buffer, 256, 0 );
+            time( &time_current );
+            printf( "%.8s Server: %s\n", ctime( &time_current ) + 11, buffer );
+        } else if( result == -1 ){
+            time( &time_current );
+            printf( "%.8s Connection error", ctime( &time_current ) + 11 );
+            break;
+        } else if( result == 1 ){
+            time( &time_current );
+            printf( "%.8s Server not responding:\n", ctime( &time_current ) + 11 );
+            if( strcmp( buffer, "close" ) == 0 ){
+                break;
+            }
+        }
+
+        memset( buffer, '\0', 256 );
+        time( &time_current );
+        printf( "%.8s    You: ", ctime( &time_current ) + 11 );
+        scanf( "%[^\n]", buffer );
+        while( ( getchar() ) != '\n' );
+        result = send( fd_serv, buffer, strlen( buffer ), 0 );
+    }
+
+    close( fd_serv );
+    time( &time_current );
+    printf("%.8s Connection closed\n", ctime( &time_current ) + 11 );
     return 0;
 }
